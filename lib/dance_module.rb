99524a26a814d067d0ab4bf3a48bea20f2ca6a79
Angelina Montanez <angelinamontanez80@gmail.com>
2019-03-24 23:19:48 +0000

    Done.

---
+++ lib/dance_module.rb
@@ -0,0 +1,42 @@
+module Dance
+end
+
+module Dance
+  def twirl
+    "I'm twirling!"
+  end
+
+  def jump
+    "Look how high I'm jumping!"
+  end
+
+  def pirouette
+    "I'm doing a pirouette"
+  end
+
+  def take_a_bow
+    "Thank you, thank you. It was a pleasure to dance for you all. "
+  end
+
+  class Kid
+  include Dance
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
+class Dancer
+  include Dance
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
+
+class Dancer
+  extend MetaDancing
+end
--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,12 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+
+class Kid
+end
+
+class Dancer
+end
+
+class Kid
+  extend MetaDancing
+end
